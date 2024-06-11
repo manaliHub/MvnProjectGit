f0c1cdc79525adbbe496cd9846b655ddedb51aff
moholkar.manali@gmail.com <moholkar.manali@gmail.com>
2024-06-11 16:08:38 +1100

    3rd commit on feature1 branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,8 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("This Maven Project Is crted from Intellij with java 11");
         System.out.printf("2nd commit");
+        System.out.printf("3nd commit on feature1 branch");
+
 
 
     }
