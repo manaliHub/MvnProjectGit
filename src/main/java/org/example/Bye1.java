0049a80bd83c72d1e0a66ed4c599da0e4601d489
moholkar.manali@gmail.com <moholkar.manali@gmail.com>
2024-06-12 16:47:55 +1100

    master commit after feature2

---
+++ src/main/java/org/example/Bye1.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Bye1 {
+    public static void main(String[] args) {
+        System.out.printf("This bye1 class from  on main branch");
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,15 +7,6 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("This Maven Project Is crted from Intellij with java 11");
-        System.out.printf("2nd commit");
-        System.out.printf("3nd commit on feature1 branch");
-        System.out.printf("4th commit on master branch");
-        System.out.printf("6th commit on master branch");
-        System.out.printf("7th commit on feature branch");
-        System.out.printf("8th commit on master branch");
-        System.out.printf("8th commit on feature branch");
-
-
     }
 
     public void newMethod(){
