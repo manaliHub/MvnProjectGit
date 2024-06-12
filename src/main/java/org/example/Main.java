6c67e343ee6502c9f500154dea1171ed9c1555cc
moholkar.manali@gmail.com <moholkar.manali@gmail.com>
2024-06-12 19:14:54 +1100

    master commit after feature2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("This Maven Project Is crted from Intellij with java 111");
         System.out.printf("This Maven Project Is crted from Intellij with java 111");
-        System.out.printf("This Maven Project Is crted from Intellij with java 12");
+        System.out.printf("This Maven Project Is crted from Intellij with java 122");
     }
 
     public void newMethod(){
